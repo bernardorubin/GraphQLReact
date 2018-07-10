b6ae55acb2cd95a386573d61ab30a8c93898b083
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2018-07-10 02:43:57 -0500

    Commit for Github

---
+++ 0-graphql_notes.js
@@ -0,0 +1,10 @@
+//-------------------//
+// Graphql
+//-------------------//
+// Graphql wants to fix inconsistencies or challenges around
+// restful routing and tackle big issues around overserving data
+
+// Restful routing has issues in 3 areas
+// 1. Deciding a url schema gets tough when we start to have heavily nested relationships
+// 2. When fetching heavily nested data we can get into situations where we make too many http requests to get the data we need
+// 3. Vulnerable to over fetching data
